c6ba8a0a3830011c731ea9d7ec927f525d58b78c
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2025-09-12 14:20:19 +0530

    07 : complete dashboard-page

--- client/src/pages/dashboardPage/Index.jsx
+++ client/src/pages/dashboardPage/Index.jsx
@@ -1,5 +1,57 @@
+import { Protect } from "@clerk/clerk-react";
+import { Gem, Sparkle } from "lucide-react";
+import { useEffect, useState } from "react";
+import { dummyCreationData } from "../../assets/assets";
+import CreationsItem from "../../components/sidebar/CreationsItem";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const [creations, setCreations] = useState([]);
+
+  const getDashboardData = async () => {
+    setCreations(dummyCreationData);
+  };
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+
+  return (
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h2 className="text-xl font-semibold">{creations.length}</h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588f2] to-[#0bb0d7] text-white flex justify-center items-center">
+            <Sparkle className="w-5 text-white" />
+          </div>
+        </div>
+        {/* active plan */}
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Active Plan</p>
+            <h2 className="text-xl font-semibold">
+              <Protect plan={"premium"} fallback="Free">
+                Premium
+              </Protect>
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#ff61c5] to-[#9e53ee] text-white flex justify-center items-center">
+            <Gem className="w-5 text-white" />
+          </div>
+        </div>
+      </div>
+
+      {/*  */}
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {creations.map((items) => {
+          return <CreationsItem key={items.id} items={items} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
